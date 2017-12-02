bbba70141e578afe744cfc4191a7c0050eff17de
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2017-12-02 02:49:03 +0100

    HasOwner interface has been added.

---
+++ storage/src/main/java/org/ctoolkit/services/storage/HasOwner.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017 Comvai, s.r.o. All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.ctoolkit.services.storage;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Declaration for an entity that's being owned by another entity.
+ * It's NOT a parent/child relationship, but represents an ownership from the business point of view.
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+public interface HasOwner<T extends EntityIdentity>
+{
+    /**
+     * Returns the owner of the entity that implements this interface.
+     *
+     * @return the owner of this entity or {@code null} if not yet set
+     */
+    T getOwner();
+
+    /**
+     * Sets the owner for this entity.
+     *
+     * @param owner the entity to be set as an owner
+     */
+    void setOwner( @Nonnull T owner );
+
+    /**
+     * Checks whether given entity is the same entity as declared owner.
+     *
+     * @param entity the entity to be checked
+     * @return {@code true} if given entity is tha same as declared owner, otherwise {@code false}.
+     */
+    boolean checkOwner( @Nonnull T entity );
+}
