f4f5700e11b709b86728fa29ad2bf888a74db7d0
Aurel Medvegy <medvegy@turnonline.biz>
2019-04-20 09:35:39 +0200

    Servlet handling uploads in to Google Cloud Storage.

--- upload/src/main/java/org/ctoolkit/services/upload/CloudStorageUploadServlet.java
+++ upload/src/main/java/org/ctoolkit/services/upload/CloudStorageUploadServlet.java
@@ -19,17 +19,31 @@
 package org.ctoolkit.services.upload;
 
 import com.google.appengine.api.appidentity.AppIdentityService;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.common.base.Strings;
 import com.google.common.io.ByteStreams;
+import com.google.common.net.HttpHeaders;
+import com.google.common.net.MediaType;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Singleton;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,9 +51,58 @@ import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.UUID;
 
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static com.google.common.net.HttpHeaders.CACHE_CONTROL;
+import static com.google.common.net.HttpHeaders.X_REQUESTED_WITH;
+import static org.ctoolkit.services.storage.appengine.blob.StorageServiceBean.STORAGE_NAME_PATTERN;
+
+/**
+ * Servlet handling uploads in to Google Cloud Storage.
+ * <p>
+ * <strong>Example of the Guice module configuration:</strong>
+ * <p>
+ * {@code CloudStorageUploadServletMultipartConfig} represents a class that extends {@link CloudStorageUploadServlet}
+ * annotated with customized {@code @MultipartConfig} configuration.
+ * <pre>
+ * public class MyServletModule
+ *         extends ServletModule
+ * {
+ *    &#64;Override
+ *    protected void configureServlets()
+ *    {
+ *        String pattern = "/api/billing/v1/storage-upload";
+ *        serve( pattern ).with( CloudStorageUploadServletMultipartConfig.class );
+ *
+ *        filter( pattern ).through( CloudStorageUploadServlet.AccessControl.class );
+ *    }
+ * }
+ * </pre>
+ * Servlet call results in a JSON response with following structure:
+ * <pre>
+ * {
+ *     "items": [
+ *         {
+ *             "fileName": "my.jpeg",
+ *             "storageName": "/gs/my-default-bucket.appspot.com/uploads/nice.jpeg",
+ *             "servingUrl": "https://lh3.googleusercontent.com/abc683.."
+ *         }
+ *     ]
+ * }
+ * </pre>
+ * <strong>Note</strong>, if target servlet is running in non default service, enrich your
+ * dispatch.yaml with following, for example:
+ * <pre>
+ *   - url: '*&#47;billing/v1/storage-upload/*'
+ *     module: my-billing
+ * </pre>
+ */
+@SuppressWarnings( "UnstableApiUsage" )
 public class CloudStorageUploadServlet
         extends HttpServlet
 {
@@ -55,14 +118,19 @@ public class CloudStorageUploadServlet
 
     private final AppIdentityService appIdentity;
 
+    private final ImagesService imageService;
+
     private String bucketName;
 
-    private String directory = "Uploads";
+    private String directory = "uploads";
 
-    public CloudStorageUploadServlet( Storage storage, AppIdentityService appIdentity )
+    public CloudStorageUploadServlet( Storage storage,
+                                      AppIdentityService appIdentity,
+                                      ImagesService imageService )
     {
         this.storage = storage;
         this.appIdentity = appIdentity;
+        this.imageService = imageService;
     }
 
     @Override
@@ -83,14 +151,75 @@ public class CloudStorageUploadServlet
         Collection<Part> parts = request.getParts();
         LOGGER.info( "Parts to be stored: " + parts );
 
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson json = gsonBuilder.create();
+
+        JsonObject root = new JsonObject();
+        JsonArray items = new JsonArray();
+        root.add( "items", items );
+        JsonObject jsonEntry;
+
         for ( Part part : parts )
         {
             String filename = fileName( part );
-            BlobInfo.Builder builder = BlobInfo.newBuilder( BlobId.of( getBucketName(), filename ) );
+            String fullPath = directory + "/" + filename;
+            BlobInfo.Builder builder = BlobInfo.newBuilder( BlobId.of( getBucketName(), fullPath ) );
             builder.setContentType( part.getContentType() );
 
-            store( part, builder.build() );
+            BlobInfo blobInfo = builder.build();
+
+            try
+            {
+                store( part, blobInfo );
+            }
+            catch ( Exception e )
+            {
+                response.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+                return;
+            }
+
+            String gStorageName = MessageFormat.format(
+                    STORAGE_NAME_PATTERN,
+                    blobInfo.getBucket(),
+                    blobInfo.getName() );
+
+            jsonEntry = new JsonObject();
+            jsonEntry.addProperty( "storageName", gStorageName );
+            jsonEntry.addProperty( "fileName", filename );
+
+            String servingUrl;
+
+            if ( isAnyImageContentType( part ) )
+            {
+                ServingUrlOptions options = ServingUrlOptions.Builder
+                        .withGoogleStorageFileName( gStorageName )
+                        .crop( false )
+                        .secureUrl( true );
+
+                options.imageSize( 200 );
+
+                try
+                {
+                    servingUrl = imageService.getServingUrl( options );
+                    jsonEntry.addProperty( "servingUrl", servingUrl );
+                }
+                catch ( Exception e )
+                {
+                    // continue without 'servingUrl' property
+                    LOGGER.error( "Get serving URL failure " + options, e );
+                }
+            }
+
+            items.add( jsonEntry );
         }
+
+        json.toJson( root, response.getWriter() );
+        MediaType jsonUtf8 = MediaType.JSON_UTF_8;
+
+        response.setStatus( HttpServletResponse.SC_CREATED );
+        //noinspection OptionalGetWithoutIsPresent
+        response.setCharacterEncoding( jsonUtf8.charset().get().name() );
+        response.setContentType( jsonUtf8.toString() );
     }
 
     private void store( Part uploaded, BlobInfo blobInfo ) throws IOException
@@ -120,7 +249,6 @@ public class CloudStorageUploadServlet
         }
         else
         {
-            @SuppressWarnings( "UnstableApiUsage" )
             byte[] bytes = ByteStreams.toByteArray( uploaded.getInputStream() );
             storage.create( blobInfo, bytes );
         }
@@ -133,15 +261,62 @@ public class CloudStorageUploadServlet
         {
             fileName = UUID.randomUUID().toString();
         }
-        return directory + "/" + fileName;
+        return fileName;
     }
 
-    private String getBucketName()
+    /**
+     * Returns the bucket name to be used as a target storage location.
+     * By default it returns project default bucket name, override to be customized.
+     *
+     * @return the bucket name
+     */
+    protected String getBucketName()
     {
         if ( bucketName == null )
         {
             bucketName = appIdentity.getDefaultGcsBucketName();
         }
         return bucketName;
+    }
+
+    boolean isAnyImageContentType( Part info )
+    {
+        String contentType = info.getContentType();
+        return contentType != null && contentType.toLowerCase().startsWith( MediaType.ANY_IMAGE_TYPE.type() );
+    }
+
+    /**
+     * These headers are added:
+     * <ul>
+     * <li>{@link HttpHeaders#ACCESS_CONTROL_ALLOW_ORIGIN}: *</li>
+     * <li>{@link HttpHeaders#ACCESS_CONTROL_ALLOW_METHODS}: OPTIONS,POST</li>
+     * <li>{@link HttpHeaders#ACCESS_CONTROL_ALLOW_HEADERS}: Cache-Control,X-Requested-With</li>
+     * </ul>
+     */
+    @Singleton
+    public static class AccessControl
+            implements Filter
+    {
+        @Override
+        public void init( FilterConfig filterConfig )
+        {
+        }
+
+        @Override
+        public void doFilter( ServletRequest request, ServletResponse servletResponse, FilterChain chain )
+                throws IOException, ServletException
+        {
+            HttpServletResponse response = ( HttpServletResponse ) servletResponse;
+
+            response.addHeader( ACCESS_CONTROL_ALLOW_ORIGIN, "*" );
+            response.addHeader( ACCESS_CONTROL_ALLOW_METHODS, "OPTIONS,POST" );
+            response.addHeader( ACCESS_CONTROL_ALLOW_HEADERS, CACHE_CONTROL + "," + X_REQUESTED_WITH );
+            chain.doFilter( request, response );
+        }
+
+        @Override
+        public void destroy()
+        {
+        }
     }
 }
---
+++ upload/src/test/java/org/ctoolkit/services/upload/CloudStorageUploadServletTest.java
@@ -0,0 +1,341 @@
+/*
+ * Copyright (c) 2019 Comvai, s.r.o. All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.ctoolkit.services.upload;
+
+import com.google.api.client.util.Charsets;
+import com.google.appengine.api.appidentity.AppIdentityService;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ServingUrlOptions;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.common.io.CharStreams;
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.Tested;
+import mockit.Verifications;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * {@link CloudStorageUploadServlet} unit testing.
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+public class CloudStorageUploadServletTest
+{
+    private static final String BUCKET = "test-bucket.appspot.com";
+
+    private static final String FILE_NAME = "nice.jpeg";
+
+    @Tested
+    private CloudStorageUploadServlet tested;
+
+    @Injectable
+    private Storage storage;
+
+    @Injectable
+    private AppIdentityService appIdentity;
+
+    @Injectable
+    private ImagesService imageService;
+
+    @Mocked
+    private HttpServletRequest request;
+
+    @Mocked
+    private HttpServletResponse response;
+
+    @Mocked
+    private Part part;
+
+    private StringWriter writer;
+
+    @BeforeMethod
+    public void before()
+    {
+        writer = new StringWriter();
+    }
+
+    @Test
+    public void upload_ImageCreated() throws ServletException, IOException, JSONException
+    {
+        expectationsWithParts();
+
+        // test call
+        tested.doPost( request, response );
+
+        new Verifications()
+        {
+            {
+                response.setContentType( "application/json; charset=utf-8" );
+                response.setCharacterEncoding( "UTF-8" );
+                response.setStatus( HttpServletResponse.SC_CREATED );
+
+                ServingUrlOptions suo;
+                imageService.getServingUrl( suo = withCapture() );
+                assertThat( suo ).isNotNull();
+
+                BlobInfo blobInfo;
+                storage.create( blobInfo = withCapture(), ( byte[] ) any );
+
+                assertThat( blobInfo ).isNotNull();
+                assertThat( blobInfo.getBucket() ).isEqualTo( BUCKET );
+                assertThat( blobInfo.getName() ).isEqualTo( "uploads/" + FILE_NAME );
+            }
+        };
+
+        String expectedResponse = fromFile( "upload-response.json" );
+        JSONAssert.assertEquals( expectedResponse, writer.toString(), JSONCompareMode.LENIENT );
+    }
+
+    @Test
+    public void upload_CreatedWithUnknownContentType() throws ServletException, IOException, JSONException
+    {
+        // no content-type
+        expectationsWithParts( null );
+
+        // test call
+        tested.doPost( request, response );
+
+        new Verifications()
+        {
+            {
+                response.setContentType( "application/json; charset=utf-8" );
+                response.setCharacterEncoding( "UTF-8" );
+                response.setStatus( HttpServletResponse.SC_CREATED );
+
+                imageService.getServingUrl( ( ServingUrlOptions ) any );
+                times = 0;
+
+                BlobInfo blobInfo;
+                //noinspection ConstantConditions
+                storage.create( blobInfo = withCapture(), ( byte[] ) any );
+
+                assertThat( blobInfo ).isNotNull();
+                assertThat( blobInfo.getBucket() ).isEqualTo( BUCKET );
+                assertThat( blobInfo.getName() ).isEqualTo( "uploads/" + FILE_NAME );
+            }
+        };
+
+        String expectedResponse = fromFile( "upload-response-min.json" );
+        JSONAssert.assertEquals( expectedResponse, writer.toString(), JSONCompareMode.LENIENT );
+    }
+
+    @Test
+    public void upload_StorageFailure() throws ServletException, IOException
+    {
+        expectationsWithParts();
+
+        new Expectations()
+        {
+            {
+                //noinspection ConstantConditions
+                storage.create( ( BlobInfo ) any, ( byte[] ) any );
+                result = new RuntimeException();
+            }
+        };
+
+        // test call
+        tested.doPost( request, response );
+
+        new Verifications()
+        {
+            {
+                response.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+            }
+        };
+    }
+
+    @Test
+    public void upload_ImageCreatedServeUrlFailure() throws ServletException, IOException, JSONException
+    {
+        expectationsWithParts();
+
+        new Expectations()
+        {
+            {
+                imageService.getServingUrl( ( ServingUrlOptions ) any );
+                result = new RuntimeException();
+            }
+        };
+
+        // test call
+        tested.doPost( request, response );
+
+        new Verifications()
+        {
+            {
+                response.setContentType( "application/json; charset=utf-8" );
+                response.setCharacterEncoding( "UTF-8" );
+                response.setStatus( HttpServletResponse.SC_CREATED );
+
+                BlobInfo blobInfo;
+                storage.create( blobInfo = withCapture(), ( byte[] ) any );
+
+                assertThat( blobInfo ).isNotNull();
+                assertThat( blobInfo.getBucket() ).isEqualTo( BUCKET );
+                assertThat( blobInfo.getName() ).isEqualTo( "uploads/" + FILE_NAME );
+            }
+        };
+
+        String expectedResponse = fromFile( "upload-response-min.json" );
+        JSONAssert.assertEquals( expectedResponse, writer.toString(), JSONCompareMode.LENIENT );
+    }
+
+    @Test
+    public void isAnyImageContentType_Jpeg()
+    {
+        new Expectations()
+        {
+            {
+                part.getContentType();
+                result = "image/jpeg";
+            }
+        };
+
+        assertThat( tested.isAnyImageContentType( part ) ).named( "Image content-type" ).isTrue();
+    }
+
+    @Test
+    public void isAnyImageContentType_Gif()
+    {
+        new Expectations()
+        {
+            {
+                part.getContentType();
+                result = "image/gif";
+            }
+        };
+
+        assertThat( tested.isAnyImageContentType( part ) ).named( "Image content-type" ).isTrue();
+    }
+
+    @Test
+    public void isAnyImageContentType_Png()
+    {
+        new Expectations()
+        {
+            {
+                part.getContentType();
+                result = "image/png";
+            }
+        };
+
+        assertThat( tested.isAnyImageContentType( part ) ).named( "Image content-type" ).isTrue();
+    }
+
+    @Test
+    public void isAnyImageContentType_Tiff()
+    {
+        new Expectations()
+        {
+            {
+                part.getContentType();
+                result = "image/tiff";
+            }
+        };
+
+        assertThat( tested.isAnyImageContentType( part ) ).named( "Image content-type" ).isTrue();
+    }
+
+    @Test
+    public void isAnyImageContentType_NonImage()
+    {
+        new Expectations()
+        {
+            {
+                part.getContentType();
+                result = "application/msword";
+            }
+        };
+
+        assertThat( tested.isAnyImageContentType( part ) ).named( "Image content-type" ).isFalse();
+    }
+
+    private void expectationsWithParts() throws IOException, ServletException
+    {
+        expectationsWithParts( "image/jpeg" );
+    }
+
+    private void expectationsWithParts( String contentType )
+            throws IOException, ServletException
+    {
+        Collection<Part> parts = new ArrayList<>();
+        parts.add( part );
+
+        new Expectations()
+        {
+            {
+                part.getInputStream();
+                result = new ByteArrayInputStream( new byte[0] );
+
+                part.getContentType();
+                result = contentType;
+
+                part.getSubmittedFileName();
+                result = FILE_NAME;
+
+                request.getParts();
+                result = parts;
+
+                appIdentity.getDefaultGcsBucketName();
+                result = BUCKET;
+
+                response.getWriter();
+                result = new PrintWriter( writer );
+                minTimes = 0;
+
+                imageService.getServingUrl( ( ServingUrlOptions ) any );
+                result = "https://cdn.google/abc683";
+                minTimes = 0;
+            }
+        };
+    }
+
+    @SuppressWarnings( "UnstableApiUsage" )
+    private String fromFile( String fileName ) throws IOException
+    {
+        InputStream stream = this.getClass().getResourceAsStream( fileName );
+        if ( stream == null )
+        {
+            String msg = fileName + " file has not been found in " + this.getClass().getPackage() + ".";
+            throw new IllegalArgumentException( msg );
+        }
+        return CharStreams.toString( new InputStreamReader( stream, Charsets.UTF_8 ) );
+    }
+}
